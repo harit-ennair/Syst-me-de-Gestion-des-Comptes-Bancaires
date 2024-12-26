9fa2deb5f813670bdfad71a69e08a04819d117c1
harit <harit@gmail.com>
2024-12-26 11:34:03 +0100

    creation-database

---
+++ sql/sql.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE DBbanque;
+USE DBbanque;
+
+CREATE TABLE account(
+    accountID INT PRIMARY KEY AUTO_INCREMENT ,
+    accountNum VARCHAR(50) UNIQUE NOT NULL,
+    holderName VARCHAR(50) NOT NULL,
+    balance DECIMAL(15,3) NOT NULL
+    );
+
+CREATE TABLE savingAccount( 
+      savingAccountID INT PRIMARY KEY AUTO_INCREMENT,
+      interestRate DECIMAL(5, 2) NOT NULL,
+      accountID INT,
+      FOREIGN KEY(accountID) REFERENCES account(accountID) ON DELETE CASCADE
+    );
+
+CREATE TABLE currentAccount( 
+      currentAcocuntID INT PRIMARY KEY AUTO_INCREMENT,
+      overdraftLimit DECIMAL(15, 3) NOT NULL,
+      accountID INT,
+      FOREIGN KEY(accountID) REFERENCES account(accountID) ON DELETE CASCADE
+    );
+
+CREATE TABLE businessAccount( 
+      businessAcocuntID INT PRIMARY KEY AUTO_INCREMENT,
+      transactionFee DECIMAL(10, 2) NOT NULL,
+      accountID INT,
+      FOREIGN KEY(accountID) REFERENCES account(accountID) ON DELETE CASCADE
+    );
+
